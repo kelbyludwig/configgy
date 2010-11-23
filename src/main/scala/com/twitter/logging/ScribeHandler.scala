7e8da5ca035f8eed01e7a13e30d2da239565857d
Robey Pointer <robeypointer@gmail.com>
2010-11-23 11:18:00 -0800

    port ScribeHandler and tests.

---
+++ src/main/scala/com/twitter/logging/ScribeHandler.scala
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2010 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.twitter
+package logging
+
+import java.io.IOException
+import java.net._
+import java.nio.{ByteBuffer, ByteOrder}
+import java.util.{Arrays, logging => javalog}
+import scala.collection.mutable
+
+private class Retry extends Exception("retry")
+
+class ScribeHandlerConfig {
+  val formatter: Formatter = BasicFormatter
+
+  // send a scribe message no more frequently than this:
+  val bufferTimeMilliseconds = 100
+
+  // don't connect more frequently than this (when the scribe server is down):
+  val connectBackoffMilliseconds = 15000
+
+  val maxMessagesPerTransaction = 1000
+  val maxMessagesToBuffer = 10000
+
+  val hostname = "localhost"
+  val port = 1463
+  val category = "scala"
+}
+
+object ScribeHandler {
+  val OK = 0
+  val TRY_LATER = 1
+}
+
+class ScribeHandler(config: ScribeHandlerConfig) extends Handler(config.formatter) {
+  // it may be necessary to log errors here if scribe is down:
+  val log = Logger.get("scribe")
+
+  var lastTransmission: Long = 0
+  var lastConnectAttempt: Long = 0
+
+  var socket: Option[Socket] = None
+  val queue = new mutable.ArrayBuffer[String]
+
+  var archaicServer = false
+
+  private def connect() {
+    val now = System.currentTimeMillis
+    if (!socket.isDefined && (now - lastConnectAttempt > config.connectBackoffMilliseconds)) {
+      lastConnectAttempt = now
+      try {
+        socket = Some(new Socket(config.hostname, config.port))
+      } catch {
+        case e: Exception =>
+          log.error("Unable to open socket to scribe server at %s:%d: %s", config.hostname, config.port, e)
+      }
+    }
+  }
+
+  def flush(): Unit = synchronized {
+    connect()
+    for (s <- socket) {
+      val outStream = s.getOutputStream()
+      val inStream = s.getInputStream()
+      val count = config.maxMessagesPerTransaction min queue.size
+      val buffer = makeBuffer(count)
+
+      try {
+        outStream.write(buffer.array)
+        val expectedReply = if (archaicServer) OLD_SCRIBE_REPLY else SCRIBE_REPLY
+
+        // read response:
+        val response = new Array[Byte](expectedReply.length)
+        var offset = 0
+        while (offset < response.length) {
+          val n = inStream.read(response, offset, response.length - offset)
+          if (n < 0) {
+            throw new IOException("End of stream")
+          }
+          offset += n
+          if (!archaicServer && (offset > 0) && (response(0) == 0)) {
+            archaicServer = true
+            close()
+            lastConnectAttempt = 0
+            log.error("Scribe server is archaic; retrying with old protocol.")
+            throw new Retry
+          }
+        }
+        if (!Arrays.equals(response, expectedReply)) {
+          throw new IOException("Error response from scribe server: " + response.toList.toString)
+        }
+        queue.trimStart(count)
+        if (queue.isEmpty) {
+          lastTransmission = System.currentTimeMillis
+        }
+      } catch {
+        case _: Retry =>
+          flush()
+        case e: Exception =>
+          log.error(e, "Failed to send %d log entries to scribe server at %s:%d", count, config.hostname, config.port)
+          close()
+      }
+    }
+  }
+
+  def makeBuffer(count: Int): ByteBuffer = {
+    val texts = for (i <- 0 until count) yield queue(i).getBytes("UTF-8")
+
+    val recordHeader = ByteBuffer.wrap(new Array[Byte](10 + config.category.length))
+    recordHeader.order(ByteOrder.BIG_ENDIAN)
+    recordHeader.put(11: Byte)
+    recordHeader.putShort(1)
+    recordHeader.putInt(config.category.length)
+    recordHeader.put(config.category.getBytes("ISO-8859-1"))
+    recordHeader.put(11: Byte)
+    recordHeader.putShort(2)
+
+    val prefix = if (archaicServer) OLD_SCRIBE_PREFIX else SCRIBE_PREFIX
+    val messageSize = (count * (recordHeader.capacity + 5)) + texts.foldLeft(0) { _ + _.length } + prefix.length + 5
+    val buffer = ByteBuffer.wrap(new Array[Byte](messageSize + 4))
+    buffer.order(ByteOrder.BIG_ENDIAN)
+    // "framing":
+    buffer.putInt(messageSize)
+    buffer.put(prefix)
+    buffer.putInt(count)
+    for (text <- texts) {
+      buffer.put(recordHeader.array)
+      buffer.putInt(text.length)
+      buffer.put(text)
+      buffer.put(0: Byte)
+    }
+    buffer.put(0: Byte)
+    buffer
+  }
+
+  def close(): Unit = synchronized {
+    for (s <- socket) {
+      try {
+        s.close()
+      } catch {
+        case _ =>
+      }
+    }
+    socket = None
+  }
+
+  def publish(record: javalog.LogRecord): Unit = synchronized {
+    if (record.getLoggerName == "scribe") return
+    queue += getFormatter.format(record)
+    while (queue.size > config.maxMessagesToBuffer) {
+      queue.trimStart(1)
+    }
+    if (System.currentTimeMillis - lastTransmission >= config.bufferTimeMilliseconds) {
+      flush()
+    }
+  }
+
+  override def toString = {
+    ("<%s level=%s hostname=%s port=%d scribe_buffer_msec=%d " +
+     "scribe_backoff_msec=%d scribe_max_packet_size=%d formatter=%s>").format(getClass.getName, getLevel,
+      config.hostname, config.port, config.bufferTimeMilliseconds, config.connectBackoffMilliseconds,
+      config.maxMessagesPerTransaction, config.formatter.toString)
+  }
+
+  val SCRIBE_PREFIX: Array[Byte] = Array(
+    // version 1, call, "Log", reqid=0
+    0x80.toByte, 1, 0, 1, 0, 0, 0, 3, 'L'.toByte, 'o'.toByte, 'g'.toByte, 0, 0, 0, 0,
+    // list of structs
+    15, 0, 1, 12
+  )
+  val OLD_SCRIBE_PREFIX: Array[Byte] = Array(
+    // (no version), "Log", reply, reqid=0
+    0, 0, 0, 3, 'L'.toByte, 'o'.toByte, 'g'.toByte, 1, 0, 0, 0, 0,
+    // list of structs
+    15, 0, 1, 12
+  )
+
+  val SCRIBE_REPLY: Array[Byte] = Array(
+    // version 1, reply, "Log", reqid=0
+    0x80.toByte, 1, 0, 2, 0, 0, 0, 3, 'L'.toByte, 'o'.toByte, 'g'.toByte, 0, 0, 0, 0,
+    // int, fid 0, 0=ok
+    8, 0, 0, 0, 0, 0, 0, 0
+  )
+  val OLD_SCRIBE_REPLY: Array[Byte] = Array(
+    0, 0, 0, 20,
+    // (no version), "Log", reply, reqid=0
+    0, 0, 0, 3, 'L'.toByte, 'o'.toByte, 'g'.toByte, 2, 0, 0, 0, 0,
+    // int, fid 0, 0=ok
+    8, 0, 0, 0, 0, 0, 0, 0
+  )
+}
