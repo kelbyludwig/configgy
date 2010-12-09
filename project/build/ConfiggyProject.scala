ccc653f67b4a5f13ad1d381ae43f23615630fb1a
Robey Pointer <robeypointer@gmail.com>
2010-12-08 18:03:39 -0800

    use scala 2.8.1.

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -2,13 +2,13 @@ import sbt._
 import com.twitter.sbt._
 
 class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
-  val json = "com.twitter" %% "json" % "2.1.4"
-  val scalaCompiler = "org.scala-lang" % "scala-compiler" % "2.8.0" % "compile"
+  val json = "com.twitter" % "json_2.8.0" % "2.1.4"
+  val scalaCompiler = "org.scala-lang" % "scala-compiler" % "2.8.1" % "compile"
 
   // workaround bug in sbt that hides scala-compiler.
   override def filterScalaJars = false
 
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
+  val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test"
 
   override def pomExtra =
     <licenses>
